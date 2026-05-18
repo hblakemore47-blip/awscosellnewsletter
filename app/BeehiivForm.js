c8e5ca3f65e7668eb7ca1b318b9b8a282e8eafb4
hblakemore47-blip <hblakemore47@googlemail.com>
2026-05-18 10:07:56 +1000

    Update BeehiivForm.js

--- app/BeehiivForm.js
+++ app/BeehiivForm.js
@@ -1,18 +1,24 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export default function BeehiivForm() {
+  const ref = useRef(null);
+
   useEffect(() => {
+    if (!ref.current) return;
     const script = document.createElement("script");
     script.src = "https://subscribe-forms.beehiiv.com/v3/loader.js";
     script.async = true;
     script.setAttribute("data-beehiiv-form", "d69decc8-4dc9-490a-aaf5-7c1cb3c98a14");
-    document.head.appendChild(script);
+    script.onload = () => {
+      if (window.beehiiv) window.beehiiv.init();
+    };
+    ref.current.appendChild(script);
   }, []);
 
   return (
     <div className="flex justify-center mb-4">
-      <div data-beehiiv-form="d69decc8-4dc9-490a-aaf5-7c1cb3c98a14"></div>
+      <div ref={ref} data-beehiiv-form="d69decc8-4dc9-490a-aaf5-7c1cb3c98a14"></div>
     </div>
   );
 }
