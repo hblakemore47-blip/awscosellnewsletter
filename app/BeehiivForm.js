bce6070fbc381a96061ddcdc3f758918658d983f
hblakemore47-blip <hblakemore47@googlemail.com>
2026-05-18 10:27:52 +1000

    Update BeehiivForm.js

--- app/BeehiivForm.js
+++ app/BeehiivForm.js
@@ -17,8 +17,8 @@ export default function BeehiivForm() {
   }, []);
 
   return (
-    <div className="flex justify-center mb-4">
-      <div ref={ref} data-beehiiv-form="d69decc8-4dc9-490a-aaf5-7c1cb3c98a14"></div>
+    <div style={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
+      <div ref={ref} data-beehiiv-form="d69decc8-4dc9-490a-aaf5-7c1cb3c98a14" style={{ width: '100%', maxWidth: '480px' }}></div>
     </div>
   );
 }
